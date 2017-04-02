5e49adc2305a8be35523c965deae4314537adf1b
Phil Dornfeld <phillydorn75@gmail.com>
2017-04-02 15:31:27 -0700

    capital L

--- app/containers/Leagues.js
+++ app/containers/Leagues.js
@@ -2,7 +2,7 @@ import React, {Component, PropTypes} from 'react';
 import {connect } from 'react-redux';
 import { loadUserLeagues } from '../actions/leagues';
 import AuthComponent from './Authenticated';
-import Leagues from '../components/leagues';
+import Leagues from '../components/Leagues';
 
 
 
